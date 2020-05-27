46c4ec43c21ec424168559f02dd41d59b009b52a
3039740 <3039740@PF367848.ifrn.local>
2020-05-27 14:15:38 -0300

    Revert "Mudado para contexto default = 4 e corrigido o problema da codificação. "
    This reverts commit 7263fe1d9a90ef3d193fc1303a530f555f60bdfc.

--- src/main/java/Compressores/TextFile.java
+++ src/main/java/Compressores/TextFile.java
@@ -1,16 +1,12 @@
 package Compressores;
 
-import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Contem métodos para manipulação de um arquivo texto.
@@ -151,25 +147,16 @@ public class TextFile {
      * @return uma String com todo o conteúdo do arquivo texto
      */
     public String getContent() throws FileNotFoundException {
-        BufferedReader reader = null;
-//        Scanner arquivo = null;
+        Scanner arquivo = null;
         String aux = "";
 
-        try {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
-//            arquivo = new Scanner(new FileInputStream(file));
-            String line = null;
+        arquivo = new Scanner(new FileInputStream(file));
 
-            while ((line = reader.readLine()) != null) {
-                aux += line + "\n";
-            }
-        
-            reader.close();
-        } catch (IOException ex) {
-            throw new FileNotFoundException(ex.getMessage());
+        while (arquivo.hasNext()) {
+            aux += arquivo.nextLine() + "\n";
         }
 
-//        arquivo.close();
+        arquivo.close();
         return aux;
     }
     
