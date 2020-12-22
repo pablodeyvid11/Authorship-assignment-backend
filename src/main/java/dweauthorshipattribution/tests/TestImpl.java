7d1f346c1a9245e333d45035cd02a89836d4a589
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-12-22 18:33:37 -0300

    Comentários 1

--- src/main/java/dweauthorshipattribution/tests/TestImpl.java
+++ src/main/java/dweauthorshipattribution/tests/TestImpl.java
@@ -20,22 +20,22 @@ import dweauthorshipattribution.persistence.dao.WordDAO;
 import dweauthorshipattribution.util.FileUtils;
 
 /**
- * Classe de testes do projeto de atribuicao de autoria.
+ * Classe main do projeto de atribuicao de autoria.
  *
- * <br><br>
- * Esse projeto faz parte de uma das atividades da disciplina de Introdução
- * à Teoria da Informação do Departamento de Informática da Universidade Federal
- * da Paraíba (UFPB) do período 2010.1 ministrada pelo professor Leonardo Vidal e
- * consiste na atribuicao de autoria utilizando a frequencia de classificacao das
- * palavras utilizadas por um determinado autor.
- * Este e' o projeto final da disciplina
+ * <br>
+ * <br>
+ * Esse projeto faz parte do projeto integrador dos alunos:
+ * Clara Yasmin Cunha Fernandes dos Santos;
+ * Pablo Deyvid de Paiva.
+ * 
+ * Orientados pelo professor mestre Elenilson Vieira.
+ * 
  *
- * @since 26 de junho de 2010
+ * @since 2020
  * @author Elenilson Vieira - elenilson[at]elenilsonvieira.com
- * @author Daniel Pires - dpsmetal[at]gmail.com
- * @author Wolgrand Cardoso - wolgrandcardoso[at]gmail.com
- * @version 1.0
- * @see www.di.ufpb.br
+ * @author Pablo Deyvid - claryasmin.cf[at]gmail.com
+ * @author Clara Yasmin - wolgrandcardoso[at]gmail.com
+ * @version 2.0
  */
 public class TestImpl {
 
